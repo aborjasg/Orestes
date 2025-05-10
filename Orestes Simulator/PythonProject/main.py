891df7003019d97e6c600aaa31cf6caba0251282
Alex BG <aborjasg@protonmail.com>
2025-05-10 15:30:55 -0700

    Added AI samples

--- Orestes Simulator/PythonProject/main.py
+++ Orestes Simulator/PythonProject/main.py
@@ -1,6 +1,8 @@
-import io
+from Functions.clue import *
+from Functions.logic import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Orestes Simulator')
+
 
